0e401d558f36705a4f6c301fde4b969a21904503
GlebNagornikov <mr.nagornikov@mail.ru>
2026-09-14 13:10:01 +0200

    Connect Django to PostgreSQL, add employees app

---
+++ employees/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=200)
+    position = models.CharField(max_length=100)
+    hired_at = models.DateField()
+
+    def __str__(self):
+        return self.full_name
+
